e85781b810badb92dbe512671cd34ea83ee56056
phlaxyr <phlaxyzerobane@gmail.com>
2016-07-07 21:40:18 -0500

    Chat Message when someone sleeps

--- src/main/java/co/gm4/GM4_BetterBeds/BetterBeds.java
+++ src/main/java/co/gm4/GM4_BetterBeds/BetterBeds.java
@@ -1,6 +1,7 @@
 package co.gm4.GM4_BetterBeds;
 
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.World;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -8,10 +9,12 @@ import org.bukkit.event.player.PlayerBedEnterEvent;
 import org.bukkit.event.player.PlayerBedLeaveEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
+
 public class BetterBeds extends JavaPlugin implements Listener {
 		
 	private static long WAKETIME;
 	private static long SLEEPDELAY;
+	private static String SLEEPINGMSG;
 	private boolean cancelSleep = false;
 	
 	@Override
@@ -20,15 +23,18 @@ public class BetterBeds extends JavaPlugin implements Listener {
 		saveDefaultConfig();
 		WAKETIME = getConfig().getLong("wake-time");
 		SLEEPDELAY = getConfig().getLong("sleep-delay");
+		SLEEPINGMSG = getConfig().getString("sleeping-msg");
 	}
 	
 	@EventHandler
 	public void enterBed(PlayerBedEnterEvent event) {
+		cancelSleep=false;
+		getServer().broadcastMessage(ChatColor.WHITE + event.getPlayer().getName() + ChatColor.YELLOW+SLEEPINGMSG);
 		getServer().getScheduler().scheduleSyncDelayedTask(this, new Runnable() {
 			public void run(){
-				cancelSleep=false;
+
 				World world = event.getBed().getWorld();
-				while (cancelSleep=false && !event.isCancelled()) {
+				if (cancelSleep=false) {
 					world.setTime(WAKETIME);
 					world.setStorm(false);
 					world.setThundering(false);
